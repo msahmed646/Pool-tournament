15171c6cda99a16c278d4da8667cdd21ef53084d
msahmed646 <msahmed646@gmail.com>
2025-08-22 12:24:52 -0400

    updated App.jsx, main.jsx, Navbar.jsx, Shuffle.jsx, useTournament and groupings.js

---
+++ pool-tournament/src/store/useTournamentStore.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand'
+
+
+const useTournamentStore = create((set, get) => ({
+players: [], // [{id, name}]
+groupsCount: 4, // multiple of 4
+groupCap: 8, // recommended max players per group
+groups: [],
+
+
+setPlayers: (list) => set({ players: list }),
+setGroupsCount: (n) => set({ groupsCount: n }),
+setGroupCap: (n) => set({ groupCap: n }),
+setGroups: (g) => set({ groups: g }),
+}))
+
+
+export default useTournamentStore
